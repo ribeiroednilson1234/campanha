4b0add20fa79afc1f5d1623377c87b02dc2a1708
Ednilson Alves <130374794+ribeiroednilson1234@users.noreply.github.com>
2025-04-01 19:08:50 -0300

    Add files via upload

---
+++ curto.php
@@ -0,0 +1,29 @@
+<?
+estudo(nome,idade);
+$N=$_post["nome"];
+$I=$_post["idade"];
+$wy=$_post["Erro"];
+	if
+		N=string;
+			echo "<p><b>Nome cadastrado: [$N];</b></p>"}
+	if{	I=int; 
+			echo "<br><p><b>Idade cadastrada: [$I];</b></p>"}
+
+else;{
+	Erro=boolean};
+	echo"$wy, <b>Dados não cadastrados.</b>";
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8" lang="Pt-Br"/>
+	<meta author="Ednilson" date="201812"/>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css"/>
+	<script type="text/javascript" src="java/curto.js"></script>
+	<title>Estudo do php java css e html5</title>
+	</head>
+<body>
+<p></p>
+<href="/campanha/enviar/index.html">VOLTAR AO INÍCIO</a>
+</body>
+</html>
