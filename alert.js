2f2aeaf91ba084dfbe3c62d56b857051af48faed
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2025-04-05 11:36:47 -0300

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,4 +1,4 @@
 ﻿<script type="text/javascript" src="/enviar/java/alert.js">
-	function=windown.onload(alert);{
-	return alert("Bem vindo ao serviço informativo ao contratante");}
+	function=windown.onload(Aviso);{
+	Aviso;("Bem vindo ao serviço informativo ao contratante");}
 	</script>
