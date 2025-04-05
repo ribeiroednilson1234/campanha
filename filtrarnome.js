da20528982601f2f37a2c170b28a5030ef97e3e2
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2025-04-05 11:08:18 -0300

    Update filtrarnome.js

--- filtrarnome.js
+++ filtrarnome.js
@@ -1,8 +1,8 @@
 //arrey para filtrar nome em aprovação lista de concurso realizado pelas bancas examinadores no Brasil.
-var nome = ["Procurado Concursado Aprovado", "Identidade.Inscrito", "CPF.Confirma"];
+var nome = ["Procurado Concursado Aprovado", "Inscrito.Identidade", "Confirma.CPF"];
 function filtrarNome(elem, ind, obj){
 	return ((typeof elem == "Procurado Concursado Aprovado"), elem = "Identidade", elem = "CPF");
 	}; 
 nome.Concursos(filtrarNome); // informa o nome e números de documentos exatamente igual
-alert(in nome);
+alert(in nome, in elem, in elem);
 break
