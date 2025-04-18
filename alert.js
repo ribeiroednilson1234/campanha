d26c5afe63ba71e297f67b826a8fb2610b667f42
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2025-04-18 10:23:11 -0300

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,4 +1,5 @@
 <script type="text/javascript" src="Campanha/enviar/java/alert.js">
-	windown.onload=function();{Aviso("<b>Bem vindo ao serviço informativo ao contratante</b>");}try{
+	windown.onload=function(){Aviso("<b>Bem vindo ao serviço informativo ao contratante</b>")}
+try{
 et d = new Date();
-document.body.innerHTML = "<h1>Confira a sua hora local e data:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + ":" + d.getMM()+ ":" + d.getDD()+ ":" + d.getAAAA()+ ":"</h1>"}</script>
+document.body.innerHTML = "<h1>Confira a sua hora local e data:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + ":" + d.getMM() + ":" + d.getDD() + ":" + d.getAAAA() + ":"</h1>"}</script>
