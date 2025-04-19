82ebcb7fc0447a4ac16f076b872b45655cfc27e9
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2025-04-19 09:18:24 -0300

    Add files via upload

---
+++ curto.php
@@ -0,0 +1,44 @@
+﻿<?
+estudo(Nome,Idade,Identidade,CPF);
+$Nome=$_post["Nome"];
+$Idade=$_post["Idade"];$Identidade=$_Post["Identidade"];
+$CPF=$_post["CPF"];
+$wy=$_post["Erro"];
+
+if
+{	Nome=string;
+	echo "<p><b>Nome cadastrado: [$Nome];</b></p>"}
+	
+if{	Idade=int;	echo "<br><p><b>Idade cadastrada: [$Idade];</b></p>"}
+
+if{	Identidade; 	echo "<br><p><b>Identidade Cadastrada: [$Identidade]"</b></p>}
+if{	CPF=int;
+	echo "<br><p><b>CPF cadastrado: [$CPF];</b></p>"}
+
+else;{
+	Erro=boolean};
+	echo"$wy, <br><b>Dados não cadastrados erro procure o administrador do sistema.</b>";
+?>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+	<meta charset="utf-8" lang="Pt-Br"/>
+
+	<meta author="Ednilson" date="201812"/>
+
+	<link rel="stylesheet" type="text/css" href="css/estilo.css"/>
+
+	<script type="text/javascript" src="Campanha/java/furto.js"></script>
+
+	<title>Concursado cadastrado</title>
+
+	</head>
+<body><article><p>Os dados foram cadastrados de acordo com o informado no formuláiro enviado</p>
+
+<href="/campanha/enviar/index.html">VOLTAR AO INÍCIO</a>
+</article>
+</body>
+
+</html>
---
+++ filtrarnome.js
@@ -0,0 +1,8 @@
+﻿//arrey para filtrar nome em aprovação lista de concurso realizado pelas bancas examinadores no Brasil.
+var nome = ["Concursado *", "Idade", "Identidade", "CPF"];
+function filtrarNome(elem, ind, obj){
+	return ((typeof elem == "Concursado_*"), elem = "Idade", elem = "Identidade", elem = "CPF");
+	}; 
+nome.Concursos(filtrarNome); // informa o nome e números de documentos exatamente igual
+alert(in nome, in elem, in elem, in elem);
+break,
