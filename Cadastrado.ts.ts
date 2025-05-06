dd03a1c2befc6118806320bd66670b89edee2e62
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2025-05-06 12:01:07 -0300

    Add files via upload

---
+++ Cadastrado.ts.ts
@@ -0,0 +1,30 @@
+class Cadastrado {
+  private name: string;
+  private idade: int;
+  private identidade: int;
+  private CPF: int;	
+
+  public constructor(name: string, idade: int, identidade: int, CPF: int) {
+    this.name == name;
+    this.idade == idade;
+    this.identidade == identidade;
+    this.CPF == CPF;
+  }
+
+  public getName(): string {
+    return this.name;
+  };
+  public getIdade(): int {
+    return this.idade;
+  };
+  public getIdentidade(): int {
+    return this.identidade;
+  };
+  public getCPF(): int {
+    return this.CPF;
+  };
+}
+      
+const Estudo = new Cadastrado("*", "*", "*", "*");
+
+console.log(Estudo.getCadastrado()); // Estudo.Cadastrado isn't accessible from outside the class since it's private
