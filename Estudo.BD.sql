d31ba1b19cff9ff5e7162ec7c266621fd1c9c576
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2025-05-07 19:03:54 -0300

    Update Estudo.BD.sql

--- Estudo.BD.sql
+++ Estudo.BD.sql
@@ -31,4 +31,4 @@ Idade_int(3) [not null],
 CPF_int(11) [not null],
 );
 GO
-SELECT DISTINCT Nome, FROM CADASTRO WHERE ESTUDO ORDER BY asc;
+SELECT DISTINCT Nome, Identidade, CPF FROM CADASTRO WHERE ESTUDO ORDER BY asc A-Z, a-Z, a-z;
