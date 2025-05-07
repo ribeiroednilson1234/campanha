ca70239b50a8d8ae15dfd6ab6ba5d822f53348a7
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2025-05-07 07:47:35 -0300

    Update filtrarnome.js

--- filtrarnome.js
+++ filtrarnome.js
@@ -1,8 +1,27 @@
 ﻿//arrey para filtrar nome em aprovação lista de concurso realizado pelas bancas examinadores no Brasil.
-var nome = ["Concursado *", "Idade", "Identidade", "CPF"];
+var nome = ["Concursado"];
 function filtrarNome(elem, ind, obj){
-	return ((typeof elem == "Concursado_*"), elem = "Idade", elem = "Identidade", elem = "CPF");
+	return (typeof elem = "Concursado");
+    nome.Concursos(filtrarNome); // informa o nome
+alert(<in nome);
 	}; 
-nome.Concursos(filtrarNome); // informa o nome e números de documentos exatamente igual
-alert(in nome, in elem, in elem, in elem);
+    
+let idade = ["Idade"];
+function filtrarIdade(elem, ind, obj){
+	return ((typeof elem = "Idade");
+    idade.Concursos(filtrarIdade); // informa a idade igual
+alert(<in idade);
+	}; 
+let identidade = ["Identidade"];
+function filtrarIdentidade(elem, ind, obj){
+	return ((typeof elem = "Identidade");
+    identidade.Concursos(filtrarIdade); // informa números de documentos
+alert(<in identidade);
+	};
+let CPF = ["CPF"];
+function filtrarCPF(elem, ind, obj){
+	return ((typeof elem = "CPF");
+    CPF.Concursos(filtrarCPF); // informa números de documentos
+alert(<in CPF);
+	};
 break,
