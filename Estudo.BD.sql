b9f794f47c33f3c7de0c827c27445164212db8d3
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2025-05-07 18:59:53 -0300

    Update Estudo.BD.sql

--- Estudo.BD.sql
+++ Estudo.BD.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE ESTUDO;
-
+CREATE DATABASE ESTUDO; GO
+if CONTRATADO{
 CREATE TABLE CONTRATADO( PRIMARY KEY id_CPF int(11)[not null],
 Nome_char(50) [not null],
 Idade_int(3) [not null],
@@ -9,7 +9,9 @@ CPF_int(11) [not null],
 
 INSERT INTO CONTRATADO[(id_Identidade,Nome,Idade,CPF)
 VALUES('','','','');
-
+GO
+};
+if CADASTRADO {
 CREATE TABLE CADASTRO( FORYGNE KEY id_Identidade varchar(18)[not null],
 Nome_char(50) [not null],
 Idade_int(3) [not null],
@@ -19,12 +21,14 @@ CPF_int(11) [not null],
 
 INSERT INTO CADASTRO[(id_Identidade,Nome,Idade,CPF)
 VALUES('','','','');
-
+GO
+};
+else {
 ALTER TABLE CADASTRO ADD/DROP(
 id_Identidade_varchar(18)[not null]
 Nome_char(50) [not null],
 Idade_int(3) [not null],
 CPF_int(11) [not null],
 );
-
+GO
 SELECT DISTINCT Nome, FROM CADASTRO WHERE ESTUDO ORDER BY asc;
