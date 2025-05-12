f7175099f407590a02587b61b2d5341e209128eb
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2025-05-11 23:40:06 -0300

    Add files via upload

---
+++ filtrarnome.js
@@ -0,0 +1,29 @@
+﻿//Para filtrar nome em aprovação lista de concurso realizado pelas bancas examinadores no Brasil.
+
+var nome = ["Concursado"];
+function filtrarNome(elem, ind, obj){
+	return (typeof elem = "*");
+    nome.Concursos(filtrarNome); // informa o nome
+alert(<in nome);
+	}; 
+    
+var idade = ["Idade"];
+function filtrarIdade(elem, ind, obj){
+	return ((typeof elem = "*");
+    idade.Concursos(filtrarIdade); // informa a idade igual
+alert(<in idade);
+	}; 
+var identidade = ["Identidade"];
+function filtrarIdentidade(elem, ind, obj){
+	return ((typeof elem = "*");
+    identidade.Concursos(filtrarIdade); // informa números de documentos
+alert(<in identidade);
+	};
+var CPF = ["CPF"];
+function filtrarCPF(elem, ind, obj){
+	return ((typeof elem = "*");
+    CPF.Concursos(filtrarCPF); // informa números de documentos
+alert(<in CPF);
+	};
+
+break,
