363b36997a088b0094064561b355251645ffd9c0
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2025-05-12 10:55:38 -0300

    Update curto.php

--- curto.php
+++ curto.php
@@ -29,28 +29,18 @@ if (!copy($Cadastrado, $Contratado)) {
     echo "falha ao copiar $Cadastrado...\n";
 }
 
-
 try
 {
-
 function [estudo] ([Nome],[Idade],[Identidade],[CPF]);{
 
 $Nome=$_post["Nome"];
-
 $Idade=$_post["Idade"];
 $Identidade=$_Post["Identidade"];
-
 $CPF=$_post["CPF"];
-
 $wy=$_post["Erro"];
 
-
-if
-{	Nome=string;
-	echo "<p><b>Nome cadastrado: [$Nome];</b></p>"};
-	
+if{	Nome=string;    echo "<p><b>Nome cadastrado: [$Nome];</b></p>"};
 if{	Idade=int;	echo "<br><p><b>Idade cadastrada: [$Idade];</b></p>"};
-
 if{	Identidade; 	echo "<br><p><b>Identidade Cadastrada: [$Identidade]"</b></p>};
 if{	CPF=int;
 	echo "<br><p><b>CPF cadastrado: [$CPF];</b></p>"};
@@ -58,7 +48,6 @@ if{	CPF=int;
 else;{
 	Erro=boolean};
 	echo"$wy, <br><b>Dados não cadastrados erro procure o administrador do sistema.</b>";
-
 
 elseif;
         $sql = "INSERT INTO CONTRATADO (Nome, Idade, Identidade, CPF) VALUES ('$nome', '$Idade','$Identidade','$CPF')";
@@ -73,18 +62,12 @@ $conn=null;
 
 	<head>
 	<meta charset="utf-8" lang="Pt-Br"/>
-
 	<meta author="Ednilson" date="201812"/>
-
 	<link rel="stylesheet" type="text/css" href="css/estilo.css"/>
-
 	<script type="text/javascript" src="Campanha/enviar/java/furto.js"></script>
-
 	<title>Concursado cadastrado</title>
-
 	</head>
 <body><article><p>Os dados foram cadastrados de acordo com o informado no formuláiro enviado.</p>
-
 <href="Campanha/enviar/index.html">VOLTAR AO INÍCIO</a>
 </article>
 </body>
@@ -92,5 +75,4 @@ $conn=null;
 header("Content-type: file/txt");
 echo $Cadastrado;
 ?>
-
 </html>
