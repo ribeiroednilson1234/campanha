121f03b2ee22409f5398848880cb9f71629eaab0
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2025-11-28 11:45:16 -0300

    Change grid column min width from 350px to 250px
    Signed-off-by: Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>

--- exemplo.ts
+++ exemplo.ts
@@ -3,6 +3,6 @@
 @ts-chek
 url[#]; public class.Exemplo{.grid-container {
       display: grid;
-      grid-template-columns: repeat(auto-fit, minmax(350px, 4fr)); /* Responsive columns */
+      grid-template-columns: repeat(auto-fit, minmax(250px, 4fr)); /* Responsive columns */
       grid-gap: 20px; /* Spacing between grid items */
     }}
