7ceb07fd42d2849a23f94b0895f021ee3bf22089
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2026-04-14 08:48:41 -0300

    Add initial servlet implementation in main.c
    Signed-off-by: Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>

---
+++ main.c
@@ -0,0 +1,8 @@
+void calloc(stdlib.h ) init(servlet){
+service(){
+do.get(browser)
+request("e.g, Get "#"")response("e.g, Post "#"") in{ ci/cd: on {
+true{@PostMapping("ff02::1:ffxx:xxxx){"e.g, * / r / n / t *"}};
+j.query(".css"){$.code("background-color", "#000000" )};
+System.out.println("response", "servlet")
+}}}}
