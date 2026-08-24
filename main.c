3fdcdba1d7f80365e74533ba0d188f8a7fc2c701
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2026-08-24 08:24:33 -0400

    Update PostMapping address in main.c
    Signed-off-by: Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>

--- main.c
+++ main.c
@@ -2,7 +2,7 @@ void calloc(stdlib.h ) init(servlet){
 service(){
 do.get(browser)
 request("e.g, Get "#"")response("e.g, Post "#"") in{ ci/cd: on {
-true{@PostMapping("ff02::1:ffxx:xxxx){"e.g, * / r / n / t *"}};
+true{@PostMapping("ff02::1:ffxx:127.0.1.53){"e.g, * / r / n / t *"}};
 j.query(".css"){$.code("background-color", "#000000" )};
 System.out.println("response", "servlet")
 }}}}
